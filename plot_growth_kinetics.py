b0f82bf79581bd4bfe7b968d49f93d3dbcfecd9b
anrde <96121893+andreplanque@users.noreply.github.com>
2022-01-14 11:26:45 +0100

    Update and rename plot.py to plot_growth_kinetics.py

--- plot_growth_kinetics.py
+++ plot_growth_kinetics.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-from data import *
+from data import days
 
 def plot_algae(days, algae):
     t = [day.day for day in days]
@@ -15,13 +15,13 @@ def plot_algae(days, algae):
         plt.title("Dunaliella tertiolecta")
 
     elif algae == "nanno":
-        plt.plot(t, [day.nano10.avg() for day in days], "r", label="10% seawater")
+        plt.plot(t, [day.nanno10.avg() for day in days], "r", label="10% seawater")
         plt.plot(t, [day.nano50.avg() for day in days], "orange", label="50% seawater")
         plt.plot(t, [day.nano100.avg() for day in days], "g", label="100% seawater")
 
-        plt.errorbar(t, [day.nano100.avg() for day in days], yerr=[day.nano100.err() for day in days], ecolor="g", fmt="none", capsize=2)
-        plt.errorbar(t, [day.nano50.avg() for day in days], yerr=[day.nano50.err() for day in days], ecolor="orange", fmt="none", capsize=2)
-        plt.errorbar(t, [day.nano10.avg() for day in days], yerr=[day.nano10.err() for day in days], ecolor="r", fmt="none", capsize=2)
+        plt.errorbar(t, [day.nanno100.avg() for day in days], yerr=[day.nanno100.err() for day in days], ecolor="g", fmt="none", capsize=2)
+        plt.errorbar(t, [day.nanno50.avg() for day in days], yerr=[day.nanno50.err() for day in days], ecolor="orange", fmt="none", capsize=2)
+        plt.errorbar(t, [day.nanno10.avg() for day in days], yerr=[day.nanno10.err() for day in days], ecolor="r", fmt="none", capsize=2)
 
         plt.title("Nannochloropsis oceanica")
 
