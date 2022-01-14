13e60fd2c643f7a02657b67968b4413a1a5f60d7
anrde <96121893+andreplanque@users.noreply.github.com>
2022-01-14 11:27:57 +0100

    Update and rename biomass.py to plot_biomass_productivity.py

--- plot_biomass_productivity.py
+++ plot_biomass_productivity.py
@@ -8,11 +8,11 @@ def biomass(days, algae):
     
 def bar(days):
     duna = [biomass(days, algae) for algae in ["duna10", "duna50", "duna100"]]
-    nano = [biomass(days, algae) for algae in ["nano10", "nano50", "nano100"]]
+    nanno = [biomass(days, algae) for algae in ["nanno10", "nanno50", "nanno100"]]
     index = np.arange(3)
     width = 0.35
     plt.bar(index, duna, width, label="Duna")
-    plt.bar(index + width, nano, width,  label="Nanno")
+    plt.bar(index + width, nanno, width,  label="Nanno")
     plt.xticks(index + width/2, ("10%", "50%", "100%"))
     plt.title("Biomass productivity")
     plt.ylabel(f"Biomass productivity\n{r'($g_{DCW}$ $L^{-1}$ $days^{-1}$)'}")
