1524dcf74d259a77551b5615e70de9f2076b89e0
anrde <96121893+andreplanque@users.noreply.github.com>
2022-01-14 11:28:54 +0100

    name consistency

--- data.py
+++ data.py
@@ -20,9 +20,9 @@ class algae():
         self.duna10 = data([26.42*(i-self.control) for i in [A1, A2, A3, A4, A5, A6]])
         self.duna50 = data([26.42*(i-self.control) for i in [B1, B2, B3, B4, B5, B6]])
         self.duna100 = data([26.42*(i-self.control) for i in [C1, C2, C3, C4, C5, C6]])
-        self.nano10 = data([7.2795*(i-self.control) for i in [D1, D2, D3, D4, D5, D6]])
-        self.nano50 = data([7.2795*(i-self.control) for i in [E1, E2, E3, E4, E5, E6]])
-        self.nano100 = data([7.2795*(i-self.control) for i in [F1, F2, F3, F4, F5, F6]])
+        self.nanno10 = data([7.2795*(i-self.control) for i in [D1, D2, D3, D4, D5, D6]])
+        self.nanno50 = data([7.2795*(i-self.control) for i in [E1, E2, E3, E4, E5, E6]])
+        self.nanno100 = data([7.2795*(i-self.control) for i in [F1, F2, F3, F4, F5, F6]])
         
 
 d0 = algae      (
