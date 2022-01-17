67e88d8e3ab95664e17bcb029d7e13ec8001a9bf
andre <96121893+andreplanque@users.noreply.github.com>
2022-01-17 21:07:33 +0100

    fixed naming

--- plot_biomass_productivity.py
+++ plot_biomass_productivity.py
@@ -11,8 +11,8 @@ def bar(days):
     nanno = [biomass(days, algae) for algae in ["nanno10", "nanno50", "nanno100"]]
     index = np.arange(3)
     width = 0.35
-    plt.bar(index, duna, width, label="Duna")
-    plt.bar(index + width, nanno, width,  label="Nanno")
+    plt.bar(index, duna, width, label="D. tertiolecta")
+    plt.bar(index + width, nanno, width,  label="N. oceanica")
     plt.xticks(index + width/2, ("10%", "50%", "100%"))
     plt.title("Biomass productivity")
     plt.ylabel(f"Biomass productivity\n{r'($g_{DCW}$ $L^{-1}$ $days^{-1}$)'}")
